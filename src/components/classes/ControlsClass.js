3d3bb3b51ce641b379980b928c0c8cc11311b739
Scott Darby <darbyscott@gmail.com>
2019-12-10 16:14:46 +0000

    revert ControlsClass.js for iphone pinch gesture

--- src/components/classes/ControlsClass.js
+++ src/components/classes/ControlsClass.js
@@ -1,11 +1,12 @@
 import OrbitControls from 'three-orbitcontrols'
 
 import CameraClass from './CameraClass'
+import RendererClass from './RendererClass'
 import BaseClass from './BaseClass'
 
 class ControlsClass extends BaseClass {
   init () {
-    this.controls = new OrbitControls(CameraClass.getInstance().camera)
+    this.controls = new OrbitControls(CameraClass.getInstance().camera, RendererClass.getInstance().renderer.domElement)
     this.controls.minDistance = 2.4
     this.controls.maxDistance = 8
     this.controls.enablePan = false
